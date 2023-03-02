5ff2d6f23f7189e6228ff69c7a8bd75073a8ac43
telkom <ryan.zaro16@gmail.com>
2023-03-02 12:14:44 +0700

    website kopi kenangan senja

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Toogle class active
+const navbarNav = document.querySelector(".navbar-nav");
+
+// Ketika hamburger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// Klik di luar sidebar untung menghilangkan nav
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
